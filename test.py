3ea117d94dbd370c518c4b75b1f510a90b14c3ad
adasilva <adasilva@student.neumont.edu>
2025-02-27 12:13:26 -0700

    Basic Image Recognition Started

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 import tensorflow as tf;
+import mss as mss;
+import numpy as np;
+import cv2;
+
 print("TensorFlow version:", tf.__version__)
+
+sct = mss.mss()
+monitor = {"top":100, "left":100 ,"width":400, "height":400}
+
+while True:
+    screenshot = sct.grab(monitor)
+    frame = np.array(screenshot)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 #hello = tf.constant('hello world')
 
