f86553cd27a8d7381b6f2e8c393307284e282939
adasilva <adasilva@student.neumont.edu>
2025-02-27 11:10:33 -0700

    test file to confirm tensorflow is up and running

---
+++ test.py
@@ -0,0 +1,8 @@
+import tensorflow as tf;
+print("TensorFlow version:", tf.__version__)
+
+#hello = tf.constant('hello world')
+
+#with tf.compat.v1.Session() as sess:
+    #result = sess.run(hello)
+    #print(result.decode())
