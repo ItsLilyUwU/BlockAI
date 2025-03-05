2e215b00bbf2a981d37b243b3d0aa301260e1892
adasilva <adasilva@student.neumont.edu>
2025-03-05 12:13:04 -0700

    Added Dataset, Added datacleaning.py used datacleaning.py to generate tetrisdata.csv

--- test.py
+++ test.py
@@ -10,8 +10,14 @@ monitor = {"top":100, "left":100 ,"width":400, "height":400}
 
 while True:
     screenshot = sct.grab(monitor)
-    frame = np.array(screenshot)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    transcriptedScreenshot = np.array(screenshot)
+    transcriptedScreenshot = cv2.cvtColor(transcriptedScreenshot, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow("Tetris Captures",transcriptedScreenshot)
+    if cv2.waitKey == ord("q"):
+        break
+
+
 
 #hello = tf.constant('hello world')
 
